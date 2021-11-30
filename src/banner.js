e2ad7ee0609bbd9f952289a3bd75bdedcd779d6e
Irie Aoi <eai@mizle.net>
2021-11-30 19:58:27 +0900

    通信・UIに関連する部分を削除

--- src/banner.js
+++ src/banner.js
@@ -13,14 +13,6 @@
 // @match       https://osapi.dmm.com/gadgets/ifr?synd=dmm&container=dmm&owner=*&viewer=*&aid=699297&*
 // @match       https://adult-client.last-origin.com/
 // @match       https://normal-client.last-origin.com/
-// @require     https://cdn-tailwindcss.vercel.app
-// @require     https://unpkg.com/lodash@4.17.21/lodash.js
-// @require     https://unpkg.com/classnames@2.3.1/index.js
-// @require     https://unpkg.com/react@17.0.2/umd/react.production.min.js
-// @require     https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js
-// @require     https://unpkg.com/react-modal@3.14.4/dist/react-modal.js
-// @require     https://unpkg.com/@headlessui/react@1.4.2/dist/headlessui.umd.development.js
-// @require     https://unpkg.com/react-hook-form@7.20.4/dist/index.umd.js
 // ==/UserScript==
 
 /**
--- src/ui/Icon.tsx
+++
@@ -1,8 +0,0 @@
-export const Icon: React.VFC = () => {
-    return (
-        <link
-            rel="stylesheet"
-            href="https://unpkg.com/bootstrap-icons@1.7.1/font/bootstrap-icons.css"
-        ></link>
-    );
-};
